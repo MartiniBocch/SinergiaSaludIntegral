3a087f6c0da3d99b5e50460020d6df8d11131014
Jennifer <jennifer_k1@hotmail.com>
2026-03-12 22:06:56 +0000

    modify root

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: 'https://github.com/MartiniBocch/SinergiaSaludIntegral',
+  base: 'https://martinibocch.github.io/SinergiaSaludIntegral/',
   plugins: [react()],
 })
